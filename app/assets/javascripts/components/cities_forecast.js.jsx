7a83bb687b7ae93ac00bf0576c03e325fda9a1d0
fuumashirou <s.matiassalgado@gmail.com>
2016-10-20 13:17:10 -0300

    Se obtienen datos desde la api desde el componente

--- app/assets/javascripts/components/cities_forecast.js.jsx
+++ app/assets/javascripts/components/cities_forecast.js.jsx
@@ -1,9 +1,28 @@
 var CitiesForecast = React.createClass({
   propTypes: {
     cityName: React.PropTypes.string,
-    countryName: React.PropTypes.string,
-    tempMin: React.PropTypes.node,
-    tempMax: React.PropTypes.node
+    countryName: React.PropTypes.string
+  },
+
+
+  componentDidMount() {
+    var that= this;
+    url= "http://api.openweathermap.org/data/2.5/weather?q="+this.props.cityName+","+this.props.countryName+"&APPID=4d1acb469df57c5a142a6040c242d91f",
+
+    fetch(url) 
+        .then(result=> {
+            result.json().then(function(data) {  
+              // console.log(data);  
+              that.setState({tempMin:data.main.temp_min});
+              that.setState({tempMax:data.main.temp_max});
+
+              console.log("result = ", data);
+            }); 
+
+        });
+  },
+  getInitialState: function(){
+    return { tempMin: '', tempMax: ''}
   },
 
   render: function() {
@@ -11,21 +30,23 @@ var CitiesForecast = React.createClass({
       <div>
         <div>City Name: {this.props.cityName}</div>
         <div>Country Name: {this.props.countryName}</div>
-        <div>Temp Min: {this.props.tempMin}</div>
-        <div>Temp Max: {this.props.tempMax}</div>
+        <div>Temp Min: {this.state.tempMin}</div>
+        <div>Temp Max: {this.state.tempMax}</div>
       </div>
     );
+    // return (
+    //   <div>
+    //     <div>City Name: {this.props.cityName}</div>
+    //     <div>Country Name: {this.props.countryName}</div>
+    //     <div>Temp Min: {this.props.tempMin}</div>
+    //     <div>Temp Max: {this.props.tempMax}</div>
+ 
+    //   </div>
+    // );
   },
 
-  fetchData() {
-      url = 'http://api.openweathermap.org/data/2.5/weather?q='+this.props.cityName+','+this.props.countryName+'&APPID=4d1acb469df57c5a142a6040c242d91f'
-      fetch(url) 
-          .then(result=> {
-            console.log("result = ", result.json());
-              // this.setState({items:result.json()});
-          });
 
-    }
+
 
 
   
